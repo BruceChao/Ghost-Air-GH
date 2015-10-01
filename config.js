659911abf61f91d15bb72b5d0a3b4e19572e56dc
Bruce Chao <bornichildren@gmail.com>
2015-10-01 21:36:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // # Ghost Configuration
-// Setup your Ghost install for various environments
-// Documentation can be found at http://support.ghost.org/config/
+// Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
+
+// Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
 
 var path = require('path'),
     config;
@@ -10,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://127.0.0.1:2369',
+        url: process.env.GHOST_ROOT_URL,
         mail: {},
         database: {
             client: 'mysql',
@@ -25,9 +26,7 @@ config = {
         },
 
         server: {
-            // Host to be passed to node's `net.Server#listen()`
             host: '0.0.0.0',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         },
         
@@ -37,7 +36,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        // Change this to your Ghost blogs published URL.
+        // Change this to your Ghost blog's published URL.
         url: 'http://localhost:2368',
 
         // Example mail config
@@ -55,6 +54,8 @@ config = {
         //  },
         // ```
 
+        // #### Database
+        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
         database: {
             client: 'sqlite3',
             connection: {
@@ -62,12 +63,16 @@ config = {
             },
             debug: false
         },
+        // #### Server
+        // Can be host & port (default), or socket
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '0.0.0.0',
+            host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         },
+        // #### Paths
+        // Specify where your content directory lives
         paths: {
             contentPath: path.join(__dirname, '/content/')
         }
@@ -87,7 +92,7 @@ config = {
             }
         },
         server: {
-            host: '0.0.0.0',
+            host: '127.0.0.1',
             port: '2369'
         },
         logging: false
@@ -108,7 +113,7 @@ config = {
             }
         },
         server: {
-            host: '0.0.0.0',
+            host: '127.0.0.1',
             port: '2369'
         },
         logging: false
@@ -136,6 +141,4 @@ config = {
     }
 };
 
-// Export config
 module.exports = config;
-
